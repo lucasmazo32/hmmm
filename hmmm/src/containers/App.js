0b84d7d2f5b271f94ba75a682d4501da34a4b4df
Lucas Mazo <lucasmazo32@gmail.com>
2020-05-26 16:44:01 -0500

    created the first page, or welcome page for the application

---
+++ hmmm/src/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Welcome from '../components/Welcome';
+import '../assets/style/App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Welcome />
+    </div>
+  );
+}

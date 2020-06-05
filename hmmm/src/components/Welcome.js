6fdd51007701b5735cf806dc9d478f81db51c789
Lucas Mazo <lucasmazo32@gmail.com>
2020-06-05 11:56:55 -0500

    refactor welcome component nodes regarding the user and client login into a new component

--- hmmm/src/components/Welcome.js
+++ hmmm/src/components/Welcome.js
@@ -8,6 +8,7 @@ import { connect } from 'react-redux';
 import logo from '../assets/images/logo.png';
 import actions from '../actions/index';
 import '../assets/style/Welcome.css';
+import WelcomeContainer from './WelcomeContainer';
 
 const { logInUser, logInClient } = actions;
 
@@ -28,20 +29,8 @@ export function Welcome({ logInClient, logInUser }) {
     <div className="welcome-container container-xl">
       <img src={logo} alt="hmmm logo" />
       <div className="welcome">
-        <div className="welcome-user">
-          <h2>Are you a user?</h2>
-          <i className="fas fa-user">
-            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" className="svg-inline--fa fa-user fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z" /></svg>
-          </i>
-          <button onClick={handleUser} id="login-user" type="button" className="btn btn-welcome">Log in</button>
-        </div>
-        <div className="welcome-client">
-          <h2>Are you a client?</h2>
-          <i className="fas fa-user-tie">
-            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-tie" className="svg-inline--fa fa-user-tie fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm95.8 32.6L272 480l-32-136 32-56h-96l32 56-32 136-47.8-191.4C56.9 292 0 350.3 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-72.1-56.9-130.4-128.2-133.8z" /></svg>
-          </i>
-          <button onClick={handleClient} id="login-client" type="button" className="btn btn-welcome">Log in</button>
-        </div>
+        <WelcomeContainer type="user" handleUser={() => handleUser()} />
+        <WelcomeContainer type="client" handleClient={() => handleClient()} />
       </div>
       <p>
         Not a user?&nbsp;
